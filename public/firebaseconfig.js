611fee6147e622c2a3ae3846387167602730fbe4
George Abreu Jr <george8794@gmail.com>
2023-11-10 16:09:59 -0500

    Update and rename firebaseconfig.test.js to firebaseconfig.js
    added forgotten firebase config

---
+++ public/firebaseconfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAwnJDlbBfD2eKAL0w4ZjC0uXaN4zU1JsQ",
+  authDomain: "eliteentries-algo.firebaseapp.com",
+  projectId: "eliteentries-algo",
+  storageBucket: "eliteentries-algo.appspot.com",
+  messagingSenderId: "877958952735",
+  appId: "1:877958952735:web:07f14b5d5e1a115023222a",
+  measurementId: "G-E126LWT5N8"
+}
--- public/firebaseconfig.test.js
+++
@@ -1,9 +0,0 @@
-export default {
-    "apiKey": "",
-    "authDomain": "",
-    "projectId": "",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": "",
-    "measurementId": ""
-  }
