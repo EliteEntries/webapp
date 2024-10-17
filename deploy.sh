49685a9179ab4df8d1a4cf6d85cc20117e5fb09c
George Abreu Jr. <george8794@gmail.com>
2024-10-16 23:44:21 -0400

    Deployment (#4)
    * cleaner deployment
    
    * clean up deploy
    
    * deployment on push
    
    * lol
    
    * credentials fix
    
    * PR Yaml added
    
    * back to dirty for now
    
    * moving to artifacts
    
    * auth gcloud
    
    * fixed gcl
    
    * Refactor deploy.sh script for Google Cloud Run deployment
    
    * let's try it in yaml then
    
    * gcloud auth in Yaml
    
    * lol
    
    * idk anymore
    
    * correct service name
    
    * quiet deploy maybe?
    
    * updated proper auth (maybe?)
    
    * auth docker manually
    
    * Refactor Docker authentication and push process
    
    * fixed region
    
    * deploy auth
    
    * login?
    
    * not the issue
    
    * github action deployed :')

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 # Define variables
 IMAGE_NAME="webapp"
-GCR_PATH="gcr.io/eliteentries-algo/$IMAGE_NAME"
+GCR_PATH="us-docker.pkg.dev/eliteentries-algo/eliteentries/$IMAGE_NAME/$IMAGE_NAME:latest"
 REGION="us-central1"
 SERVICE_NAME="webapp"
+
+# Authenticate to Google Cloud
+gcloud auth login 
+gcloud auth configure-docker
 
 # Build, tag, and push the Docker image
 docker build -t $IMAGE_NAME . &&
@@ -13,7 +17,7 @@ docker push $GCR_PATH
 
 # Deploy to Google Cloud Run
 gcloud run deploy $SERVICE_NAME \
-    --image $GCR_PATH \
-    --platform managed \
-    --region $REGION \
-    --allow-unauthenticated
+  --image $GCR_PATH \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated
