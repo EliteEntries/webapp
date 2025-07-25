d7ae7c6cbedc45ea57f9d908e1603a27aa5d9c8a
George Abreu Jr. <george8794@gmail.com>
2025-07-25 03:30:39 +0000

    fix: remove unnecessary margin from Connections and Home components for improved layout

--- new/src/app/page.tsx
+++ new/src/app/page.tsx
@@ -4,7 +4,7 @@ import HomeButton from "./components/HomeButton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-full font-sans bg-gray-50 p-8 pt-24">
+    <div className="flex flex-col items-center h-full font-sansp-8 pt-24">
       <Image
         className="dark:invert mb-6"
         src="/icon.png"
@@ -13,7 +13,7 @@ export default function Home() {
         height={38}
         priority
       />
-      <p className="text-lg text-center max-w-xl mb-8 text-gray-800">
+      <p className="text-lg text-center max-w-xl mb-8">
         Elite Entries is a trading API key aggregator and manager for the Elite Entries trading system, built on <a href="https://redbtn.io" className="underline text-blue-600" target="_blank" rel="noopener noreferrer">redbtn</a>. Securely connect, manage, and monitor your trading API keys in one place.
       </p>
       <HomeButton />
