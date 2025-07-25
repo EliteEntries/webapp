ee7501c7e035e3a14673ff75b9249c821725cda1
George Abreu Jr. <george8794@gmail.com>
2025-07-25 03:39:38 +0000

    fix: add top margin to ConnectionsPage for improved spacing

--- new/src/app/connections/page.tsx
+++ new/src/app/connections/page.tsx
@@ -5,7 +5,7 @@ import Connections from "./components/Connections";
 export default function ConnectionsPage() {
   return (
     <AuthGuard>
-      <div className="max-w-xl mx-auto p-8 ">
+      <div className="max-w-xl mx-auto p-8 mt-12">
         <h1 className="text-2xl font-bold mb-6">Connections</h1>
         <p className="mb-8 text-gray-500 dark:text-gray-400">Aggregate your API keys for various exchanges and services below.</p>
         <Connections />
