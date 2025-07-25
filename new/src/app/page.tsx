27198d6b96677668f867decf9e5b5adae2ffcbc3
George Abreu Jr. <george8794@gmail.com>
2025-07-25 17:36:53 +0000

    fix: update logo import path in page.tsx to use relative path

--- new/src/app/page.tsx
+++ new/src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import Image from "next/image";
-import logo from "@/public/logo.png";
+import logo from "/public/logo.png";
 import HomeButton from "./components/HomeButton";
 
 export default function Home() {
